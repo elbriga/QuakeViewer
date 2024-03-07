06fc8658982a943fef68693239755eb96f4173fd
elbriga <gabrielortizlour@gmail.com>
2024-03-07 17:07:40 -0300

    init 3d

---
+++ 3d.c
@@ -0,0 +1,31 @@
+#include <math.h>
+
+#include "3d.h"
+
+int FOV = 120;
+
+void projecao3D(ponto *p)
+{
+    p->sX = (FOV * p->x) / p->z;
+    p->sY = (FOV * p->y) / p->z;
+}
+
+void rotacao2D(ponto *p, int angulo)
+{
+    p->rotSX = cos(angulo) * p->sX - sin(angulo) * p->sY;
+    p->rotSY = sin(angulo) * p->sX + cos(angulo) * p->sY;
+}
+
+void rotacao3D(ponto *p, int angulo)
+{
+    ponto pAux;
+
+    pAux.sX = p->z;
+    pAux.sY = p->x;
+    
+    p->rotX = cos(angulo) * p->sX - sin(angulo) * p->sY;
+    p->rotY = sin(angulo) * p->sX + cos(angulo) * p->sY;
+    p->rotZ = sin(angulo) * p->sX + cos(angulo) * p->sY;
+}
+
+
---
+++ 3d.h
@@ -0,0 +1,15 @@
+#ifndef gfx3D_H
+#define gfx3D_H
+
+typedef struct
+{
+    int x, y, z;
+    int rotX, rotY, rotZ;
+
+    int sX, sY;
+    int rotSX, rotSY;
+
+    int r, g, b;
+} ponto;
+
+#endif
